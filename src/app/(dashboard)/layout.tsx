659b139b6a6a8ec0964739b1939c529d4c346d73
mubeenyasin72 <mubeenyasin75@gmail.com>
2024-09-12 16:03:00 +0500

    side menu and navbar is completed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -8,15 +10,20 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      {/* LEFT */}
-      <div className="w-[16%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        <Link href="/">
+      <div className="w-[16%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-normal lg:justify-start gap-2"
+        >
           <Image src={"/logo.png"} alt="logo" width={32} height={32} />
-          <span>School Name</span>
+          <span className="hidden lg:block">School Name</span>
         </Link>
+        <Menu />
       </div>
-      {/* RIGHT */}
-      <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-green-200">Right</div>
+      <div className="w-[84%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
-} 
+}
