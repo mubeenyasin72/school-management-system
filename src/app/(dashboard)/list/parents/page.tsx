335e9e3697f05b62b0a290fd117cf96fded2bf3a
mubeenyasin72 <mubeenyasin75@gmail.com>
2024-09-24 13:38:53 +0500

    working on parent list

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,6 +1,12 @@
+import Pagination from "@/components/Pagination";
+import TabelSearch from "@/components/TabelSearch";
+import Table from "@/components/Table";
+import Image from "next/image";
+import Link from "next/link";
+import { role, parentsData } from "@/lib/data"
 const ParentList = () => {
   return (
-    <div className=''>ParentList</div>
+    <div className=''>Parent List</div>
   )
 }
 
